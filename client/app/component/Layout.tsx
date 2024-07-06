49b5e75e12cc762f0af32bb35df2050fd1178b52
NEBULA <nebula@NEBULA.local>
2024-07-06 21:56:27 +0700

    feat: add side props in layout

--- client/app/component/Layout.tsx
+++ client/app/component/Layout.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { ToastProvider } from './toast/ToastAlert'
 import Sidebar from './UI/Sidebar'
 import RightPanel from './UI/RightPanel'
+import { GetMeService } from 'service/GetMe.service'
+import LoadingSpinner from './skeletons/LoadingSpinner'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+    const { data } = GetMeService()
+
     return (
         <div className="flex flex-row">
-            <Sidebar />
+            {data ? <Sidebar /> : null}
             <div className="flex-1 m-4"> {children}</div>
-            <RightPanel />
+            {data ? <RightPanel /> : null}
             <ToastProvider />
         </div>
     )
